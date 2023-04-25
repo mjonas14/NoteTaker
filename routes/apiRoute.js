7750a2f190f8df0923df25e25e44fa3baa7d23b3
Marcus Jonas <marcusjonas@Marcuss-MacBook-Pro.local>
2023-04-24 23:20:34 -0700

    Init heroku app

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const fileData = require("../db/db.json");
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
-function readDb() {
-    readFromFile('./db/db.json');
-};
+// function readDb() {
+//     readFromFile("./db/db.json");
+// };
 
 router.get("/notes", (req, res) => res.json(fileData));
 
@@ -18,8 +18,8 @@ router.post("/notes", (req, res) => {
       text,
     };
 
-    readAndAppend(newNote, "./db/db.json");
-    readDb();
+    readAndAppend(newNote, fileData);
+    // readDb();
 
     const response = {
       status: "success",
@@ -30,12 +30,7 @@ router.post("/notes", (req, res) => {
   } else {
     res.json("Error in posting note");
   }
-
-  //   const data = req.body;
-  //   const db = fs.readFileSync('../db/db.json');
-  //   const dbItems = JSON.parse(db);
-  //   dbItems.push(data);
-  //   fs.writeFileSync('../db/db.json', JSON.stringify(dbItems));
 });
 
 module.exports = router;
+
